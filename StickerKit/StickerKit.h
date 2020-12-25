18281b3fc8543bd5869d4f1d4a366d4aeb77592d
Rudd Fawcett <rudd.fawcett@gmail.com>
2020-12-25 15:19:54 -0500

    Initial Commit

---
+++ StickerKit/StickerKit.h
@@ -0,0 +1,18 @@
+//
+//  StickerKit.h
+//  StickerKit
+//
+//  Created by Rudd Fawcett on 12/25/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StickerKit.
+FOUNDATION_EXPORT double StickerKitVersionNumber;
+
+//! Project version string for StickerKit.
+FOUNDATION_EXPORT const unsigned char StickerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StickerKit/PublicHeader.h>
+
+
